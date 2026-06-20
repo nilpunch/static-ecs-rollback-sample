26cdb93c0ae208f6f31c5baa8dd44333705571a4
Daniil Pankevich <daniil.pankevich@gmail.com>
2026-06-20 20:45:55 +0300

    Activate --help

--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -4,71 +4,65 @@ using Game.Core;
 using Shenanicode.Rollback;
 using Shenanicode.Rollback.LiteNetLib;
 
-Option<FileInfo> fileOption = new("--file")
-{
-	Description = "The file to read and display on the console",
-	Arity = ArgumentArity.ZeroOrOne,
-};
-
 Option<ushort> portOption = new("--port")
 {
 	Description = "The port to listen on",
 	Arity = ArgumentArity.ExactlyOne,
-	Required = true
+	Required = true,
 };
 
-RootCommand rootCommand = new("Server app");
+Option<FileInfo> fileOption = new("--file")
+{
+	Description = "The save file to populate the world",
+	Arity = ArgumentArity.ZeroOrOne,
+};
+
+RootCommand rootCommand = new("Game server.");
 rootCommand.Options.Add(fileOption);
 rootCommand.Options.Add(portOption);
 
-var parseResult = rootCommand.Parse(args);
-if (parseResult.Errors.Count != 0)
-{
-	foreach (var parseError in parseResult.Errors)
+rootCommand.SetAction(RunProgram);
+
+return rootCommand.Parse(args).Invoke();
+
+async Task RunProgram(ParseResult parseResult, CancellationToken arg2) {
+	var running = true;
+	Console.CancelKeyPress += (_, e) =>
 	{
-		Console.Error.WriteLine(parseError.Message);
+		e.Cancel = true;
+		running = false;
+	};
+	AppDomain.CurrentDomain.ProcessExit += (_, _) =>
+	{
+		running = false;
+	};
+
+	var clientListener = new LiteNetLibRemoteClientListener(parseResult.GetValue(portOption));
+
+	Server.Create(GameSessionSetup.SessionConfig, clientListener);
+	GameSessionSetup.Register();
+	Server.Initialize();
+
+	GameWorldSetup.Create();
+	GameWorldSetup.Register();
+	GameWorldSetup.Initialize();
+
+	Console.WriteLine($"Hello, Static World!");
+
+	clientListener.Start();
+
+	var stopwatch = Stopwatch.StartNew();
+	while (running) {
+		Server.Update(stopwatch.Elapsed.TotalSeconds);
+		await Task.Yield();
 	}
-	return 1;
+
+	clientListener.Stop();
+
+	GameWorldSetup.Destroy();
+	Server.Destroy();
+
+	Console.WriteLine($"Exit with {stopwatch.Elapsed.TotalSeconds} seconds");
 }
-
-var running = true;
-Console.CancelKeyPress += (_, e) =>
-{
-	e.Cancel = true;
-	running = false;
-};
-AppDomain.CurrentDomain.ProcessExit += (_, _) =>
-{
-	running = false;
-};
-
-var clientListener = new LiteNetLibRemoteClientListener(parseResult.GetValue(portOption));
-
-Server.Create(GameSessionSetup.SessionConfig, clientListener);
-GameSessionSetup.Register();
-Server.Initialize();
-
-GameWorldSetup.Create();
-GameWorldSetup.Register();
-GameWorldSetup.Initialize();
-
-Console.WriteLine($"Hello, Static World!");
-
-clientListener.Start();
-
-var stopwatch = Stopwatch.StartNew();
-while (running) {
-	Server.Update(stopwatch.Elapsed.TotalSeconds);
-	await Task.Yield();
-}
-
-clientListener.Stop();
-
-GameWorldSetup.Destroy();
-Server.Destroy();
-
-Console.WriteLine($"Exit with {stopwatch.Elapsed.TotalSeconds} seconds");
-
-return 0;
 
 public abstract class Server : Server<GameWorld>;
