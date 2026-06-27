2c9c2519391aa7837ee29985c93935ce6bb989fa
Daniil Pankevich <daniil.pankevich@gmail.com>
2026-06-27 14:19:56 +0300

    Reg systems

--- Unity/Assets/Game/Runtime/Core/Simulation/SimulationSetup.cs
+++ Unity/Assets/Game/Runtime/Core/Simulation/SimulationSetup.cs
@@ -4,6 +4,9 @@ namespace Game.Core.Simulation {
 	public static class SimulationSetup {
 		public static void Register() {
 			W.SetResource(new BroadPhase(200, 100, FVector2.One));
+
+			Systems.Add(new MoveSystem());
+			Systems.Add(new BroadPhaseSystem());
 		}
 	}
 }
